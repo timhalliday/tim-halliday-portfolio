992bc98b78e3a83286e16baf63844da8d4c6ce79
Tim Halliday <thalliday793@gmail.com>
2023-09-20 10:49:38 +0100

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,37 +3,21 @@ import './footer.css'
 
 export default function Footer() {
   return (
-    <footer className="footer">Footer
+    <footer className="footer">
       <div className="footer__container container">
-        <h1 className="footer_title">Tim Halliday</h1>
-      </div>
+        <div className="footer__social">
+          <a href="https://www.linkedin.com/in/timothyhalliday" className="footer__social-link" target="_blank" rel="noreferrer">
+            <i className="bx bxl-linkedin"></i>
+          </a>
 
-      <ul className="footer__list">
-        <li>
-          <a href="#home" className="footer__link">Home</a>
-        </li>
-        <li>
-          <a href="#skills" className="footer__link">Skills</a>
-        </li>
-        <li>
-          <a href="#services" className="footer__link">Services</a>
-        </li>
-        <li>
-          <a href="#projects" className="footer__link">Projects</a>
-        </li>
-        <li>
-          <a href="#contact" className="footer__link">Contact</a>
-        </li>
-      </ul>
+          <a href="https://github.com/timhalliday" className="footer__social-link" target="_blank" rel="noreferrer">
+            <i className="bx bxl-github"></i>
+          </a>
+        </div>
 
-      <div className="footer__social">
-        <a href="https://www.linkedin.com/in/timothyhalliday" className="home__social-icon" target="_blank" rel="noreferrer">
-          <i className="uil uil-linkedin-alt"></i>
-        </a>
-
-        <a href="https://github.com/timhalliday" className="home__social-icon" target="_blank" rel="noreferrer">
-          <i className="uil uil-github-alt"></i>
-        </a>
+        <span className="footer__copy">
+          &#169; Tim Halliday. All rights reserved.
+        </span>
       </div>
     </footer>
   )
