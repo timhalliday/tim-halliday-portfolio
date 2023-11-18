5e6390dff966ee6588757c905ad0681af536d6fa
Tim Halliday <thalliday793@gmail.com>
2023-11-18 15:05:07 +0000

    move img hosting to imgur

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -6,7 +6,7 @@ export default {
         "id" : "1",
         "title" : "Wordle Clone",
         "subtitle" : "Built in React",
-        "imgUrl" : "src/assets/Reactle.png",
+        "imgUrl" : "https://i.imgur.com/8nxWGGu.png",
         "description" : "I wanted to get some hands-on experience in React, so I recreated one of my favourite word games: Wordle! This mini web app is designed to mimic Wordle closely in most ways - except you can play as many times as you like in a day. I developed my skills building custom React hooks, working with JSON servers and the implementation of the Effect hook.",
         "url" : "https://timely-piroshki-f7364b.netlify.app/"
       },
